6b87ec2151ce0b59f3a05c429e18ef59cb833e4f
Randy <randypriv@gmail.com>
2021-01-23 21:25:25 +0800

    add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import { Provider } from 'next-auth/client'
 import { ChakraProvider } from "@chakra-ui/react"
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
 
     <Provider session={pageProps.session}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </QueryClientProvider>
     </Provider>
   )
 }
