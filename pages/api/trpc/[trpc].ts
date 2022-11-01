e83f1325ddd96e67947746dfbab29fea025be14c
Randy <randypriv@gmail.com>
2022-11-02 01:28:12 +0800

    remove unuse code

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,6 +1,5 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { rootRouter } from "../../../routers/root";
-import { getIronSession } from "iron-session/edge";
 import { logtoClient } from "../../../utils.server";
 // export API handler
 export default logtoClient.withLogtoApiRoute(
