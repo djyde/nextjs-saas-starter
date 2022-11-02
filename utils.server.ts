e39925ff9ec398c0daf548cce130a8cf7ec8199c
Randy <randypriv@gmail.com>
2022-11-02 15:03:56 +0800

    remove unused

--- utils.server.ts
+++ utils.server.ts
@@ -1,6 +1,3 @@
-import LogtoClient from "@logto/next";
-import { resolvedConfig } from "./config.server";
-
 export const singleton = async <T>(id: string, fn: () => Promise<T>) => {
   if (process.env.NODE_ENV === "production") {
     return await fn();
@@ -22,12 +19,3 @@ export const singletonSync = <T>(id: string, fn: () => T) => {
     return global[id] as T;
   }
 };
-
-export const logtoClient = new LogtoClient({
-  endpoint: "https://logto.randysoft.org",
-  appId: resolvedConfig.logto.appId,
-  appSecret: resolvedConfig.logto.appSecret,
-  baseUrl: resolvedConfig.baseUrl(), // 你可以修改为自己真实的 URL
-  cookieSecret: resolvedConfig.logto.cookieSecret, // Logto 自动帮你生成的 32 位密钥
-  cookieSecure: process.env.NODE_ENV === "production",
-});
