2ff59546874dd7f5b2e128515dbece6d9b879fea
Randy <randypriv@gmail.com>
2023-10-23 14:00:30 +0800

    clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppType } from 'next/app';
+import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from '../utils/trpc';
 import '../main.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <ClerkProvider>
+    <Component {...pageProps} />;
+  </ClerkProvider>
 };
 
 export default trpc.withTRPC(MyApp);
