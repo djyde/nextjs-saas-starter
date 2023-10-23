5a6aa3b105fed13ad37908a0e85add221eda10d1
Randy <randypriv@gmail.com>
2023-10-23 13:48:35 +0800

    tailwind

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+// pages/_app.js
+import '../main.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,tsx,ts}",
+  ],
+  theme: {
+    container: {
+      center: true
+   },
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+  ],
+}
